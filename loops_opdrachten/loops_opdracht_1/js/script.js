308daae36bcae14e9e67527ad47ea88e4b6fc608
Lucien van Essen <2113629@talnet.nl>
2019-11-26 13:52:44 +0100

    opdracht 1 loops

--- loops_opdrachten/loops_opdracht_1/js/script.js
+++ loops_opdrachten/loops_opdracht_1/js/script.js
@@ -1,1 +1,26 @@
 //javascript code
+
+/*
+FE-OP3-les3_conditions_loops - opdracht 1
+Schrijf een programma dat loopt door de getallen 1 t/m 5 met een for-loop.
+
+A)	Alle getallen toon je onder elkaar op het scherm via index.html. 
+B)	Alle getallen toon je met behulp van console.log() in je console
+
+Commit en push de opdracht naar GitHub
+
+for(statement 1; statement 2; statement 3){
+
+}
+
+teller++
+teller = teller + 1;
+teler += 1;
+
+while
+*/
+
+for(var teller = 1 ; teller <= 5 ; teller++){
+    console.log(teller);
+
+}
