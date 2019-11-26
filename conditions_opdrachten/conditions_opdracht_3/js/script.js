fa5dead897e8776de76c5765f011805c050f82c3
Lucien van Essen <2113629@talnet.nl>
2019-11-26 12:45:38 +0100

    opdracht 4 klaar

--- conditions_opdrachten/conditions_opdracht_3/js/script.js
+++ conditions_opdrachten/conditions_opdracht_3/js/script.js
@@ -1,1 +1,8 @@
 //javascript code
+function checkSaldo() {
+    var geldOpname = document.getElementById("geldOpname").value;
+    alert(geldOpname);
+    
+    var bankSaldo = 1000;
+    
+}
--- conditions_opdrachten/conditions_opdracht_4/js/script.js
+++ conditions_opdrachten/conditions_opdracht_4/js/script.js
@@ -1,1 +1,8 @@
 //javascript code
+
+var myNumber ;
+
+myNumber = 1234567;
+
+alert("de variablere myNumber heeft de waarden: " + myNumber);
+
