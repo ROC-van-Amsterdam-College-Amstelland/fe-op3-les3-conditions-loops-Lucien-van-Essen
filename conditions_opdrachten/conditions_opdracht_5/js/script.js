87318e207b63e07c05acc281347f9db61eceac74
Lucien van Essen <2113629@talnet.nl>
2019-11-26 13:21:53 +0100

    opdracht 5

--- conditions_opdrachten/conditions_opdracht_5/js/script.js
+++ conditions_opdrachten/conditions_opdracht_5/js/script.js
@@ -1,1 +1,23 @@
 //javascript code
+
+var myOperator = "+";
+
+var myNumber1 = prompt ("geeft getal aub");
+
+var myNumber2 = prompt ("geeft getal aub");
+
+var resultaat;
+
+if (myOperator == "+") {
+
+    resultaat = Number(myNumber1) + Number(myNumber2);
+
+    document.getElementById("resultaat").innerHTML = resultaat;
+    
+}    else if (myOperator == "-") {
+
+    resultaat = myNumber1 - myNumber2;
+
+    document.getElementById("resultaat").innerHTML = resultaat;
+
+}
