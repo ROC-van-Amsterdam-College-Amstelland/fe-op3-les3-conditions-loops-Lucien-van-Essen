fd91e5a8d9bef2d8ca5f8465269f97be23391b10
Lucien van Essen <2113629@talnet.nl>
2019-12-03 12:46:01 +0100

    opdracht 3 loops; loop die de 20 overslaat

--- loops_opdrachten/loops_opdracht_3/js/script.js
+++ loops_opdrachten/loops_opdracht_3/js/script.js
@@ -1,1 +1,9 @@
 //javascript code
+
+for(var teller = 1 ; teller <= 50 ; teller++){
+
+    if (teller === 20) { continue; }
+
+    console.log(teller);
+    document.getElementById("teller").innerHTML +=  teller;
+}
